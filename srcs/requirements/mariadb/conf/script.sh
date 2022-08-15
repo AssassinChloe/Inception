9c3a30c1a5716e14caa58ae1e5f85f890b0dbabf
Chloe ASSASSIN <cassassi@e1r3p11.clusters.42paris.fr>
2022-08-15 15:29:56 +0200

    .

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -6,23 +6,20 @@
 #    By: cassassi <cassassi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/04 17:19:51 by cassassi          #+#    #+#              #
-#    Updated: 2022/08/15 14:32:48 by cassassi         ###   ########.fr        #
+#    Updated: 2022/08/15 15:28:07 by cassassi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
 /usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
-/usr/bin/mysqld --user=root --datadir=var/lib/mysql && sleep 2
-mysql -e "CREATE DATABASE IF NOT EXIST ${MYSQL_DATABASE};"
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
 
-mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
-
-mysql -e "DELETE FROM mysql.user WHERE user=''"
-mysql -e "DELETE FROM mysql.user WHERE user='root'"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 
 pkill mysqld
-/usr/bin/mysqld --user=root --datadir=var/lib/mysql
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
